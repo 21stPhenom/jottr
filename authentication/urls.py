521dee1fe34ba9fdd12ccab9646363b9b73bc87c
21stPhenom <phenom.h.acks21@gmail.com>
2023-07-25 01:39:23 +0100

    added authentication views and URLs

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from authentication.views import home, register, login_view, logout_view
 
 app_name = 'authentication'
 
 urlpatterns = [
-    
+    path('', home, name='home'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout_view')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,84 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class HomeView(View):
+    template_name = 'authentication/home.html'
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        context = {
+            'user': request.user if request.user.is_authenticated else ''
+        }
+        return render(request, self.template_name, context)
+    
+class RegisterView(View):
+    template_name = 'authentication/register.html'
+    success_url = 'authentication:login'
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        context = {}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username).exists():
+            print("Username taken!")
+            return redirect('authentication:register')
+
+        if User.objects.filter(email=email).exists():
+            print("Email already exists!")
+            return redirect('authentication:register')
+        
+        if password1 != password2:
+            print("Passwords don't match!")
+            return redirect('authentication:register')
+
+        new_user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password1
+        )
+
+        print(f"User '{new_user.username}' created")
+        return redirect(self.success_url)
+
+class LoginView(View):
+    template_name = 'authentication/login.html'
+    success_url = 'authentication:home'
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        context = {}
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        email_or_username = request.POST['email_or_username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=email_or_username).exists():
+            user = User.objects.get(username=email_or_username)
+        elif User.objects.filter(email=email_or_username).exists():
+            user = User.objects.get(email=email_or_username)
+        
+        user_object = authenticate(username=user.username, password=password)
+        login(request, user)
+        print("Logged in")
+
+        return redirect(self.success_url)
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        logout(request)
+        return redirect('authentication:home')
+    
+home = HomeView.as_view()
+register = RegisterView.as_view()
+login_view = LoginView.as_view()
+logout_view = LogoutView.as_view()
