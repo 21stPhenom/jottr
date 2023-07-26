d0b60d14da41d35459ac49312e6c332540d721f3
21stPhenom <phenom.h.acks21@gmail.com>
2023-07-26 12:23:22 +0100

    created Note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="Note Title", max_length=200)
+    content = models.CharField(verbose_name="Note Content", max_length=10000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    short_title = models.CharField(verbose_name="Note Slug", max_length=12)
+
+    def __str__(self):
+        return f"{self.title}"
