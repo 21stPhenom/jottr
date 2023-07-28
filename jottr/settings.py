219bcea82e952e789590ba9a97ee625e56eb66ea
21stPhenom <phenom.h.acks21@gmail.com>
2023-07-28 23:42:33 +0100

    changed db to postgres

--- jottr/settings.py
+++ jottr/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'jottr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
